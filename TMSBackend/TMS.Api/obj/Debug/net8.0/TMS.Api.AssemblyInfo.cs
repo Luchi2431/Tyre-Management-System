8812fdef98589af4e9dea1547040efac3aa9c936
Luka Vlatkovic <vlatkovicluka01@gmail.com>
2024-10-17 21:42:03 +0200

    update
    update

--- TMSBackend/TMS.Api/obj/Debug/net8.0/TMS.Api.AssemblyInfo.cs
+++ TMSBackend/TMS.Api/obj/Debug/net8.0/TMS.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMS.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28bbd6bc2de654124c5f80a1e4eeae3b31372d0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+685b00e28803c78cb5f7ab6f6e1eb5062ac06834")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMS.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMS.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
