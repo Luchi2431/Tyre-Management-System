62b6f8ac0a575af0c8539ff2e58398760cee6158
Luchi <vlatkovicluka01@gmail.com>
2024-10-21 11:54:09 +0200

    visual update
    update

--- TMSBackend/TMS.Data/obj/Debug/net8.0/TMS.Data.AssemblyInfo.cs
+++ TMSBackend/TMS.Data/obj/Debug/net8.0/TMS.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMS.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93ea323a0acad11b94ff77ad7b17198ae07d13eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a155bf76ffe22ec3ba71ad21b728988b340c27a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMS.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMS.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
